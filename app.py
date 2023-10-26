e07a5447c71f48b14315e2c3a8afde6ba63cf7ee
Christos Magganas <cmagganas@users.noreply.huggingface.co>
2023-10-26 19:14:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,20 @@ import chainlit as cl
 
 import build_langchain_vector_store
 
-exec(open("build_langchain_vector_store.py").read())
+build_langchain_vector_store
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_base = 'https://api.openai.com/v1' # default
+
+embedding_model_name = "text-embedding-ada-002"
+embedding_model = OpenAIEmbeddings(model=embedding_model_name)
+read_vector_store = Chroma(
+        persist_directory="langchain-chroma-pulze-docs", embedding_function=embedding_model
+    )
+query_results = read_vector_store.similarity_search("How do I use Pulze?")
+print(query_results[0].page_content)
